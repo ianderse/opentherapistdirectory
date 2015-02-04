1fbca13b72f202f529957f89f5fa513134657c19
Ian Andersen <ianderse@mac.com>
2015-02-04 11:57:47 -0700

    cleanup routes file

--- config/routes.rb
+++ config/routes.rb
@@ -7,32 +7,35 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get 'facilities',        to: 'facilities#index',  as: 'facilities', defaults: {format: :json}
-      get 'facilities/:id',    to: 'facilities#show',   as: 'facility', defaults: {format: :json}
-      get 'therapists',        to: 'therapists#index',  as: 'therapists', defaults: {format: :json}
-      get 'therapists/:id',    to: 'therapists#show',   as: 'therapist', defaults: {format: :json}
+      get 'facilities',         to: 'facilities#index',          as: 'facilities', defaults: {format: :json}
+      get 'facilities/:id',     to: 'facilities#show',           as: 'facility', defaults: {format: :json}
+      get 'therapists',         to: 'therapists#index',          as: 'therapists', defaults: {format: :json}
+      get 'therapists/:id',     to: 'therapists#show',           as: 'therapist', defaults: {format: :json}
     end
   end
 
   namespace :admin do
-  	get 'dashboard',       to: 'dashboard#show',    as: 'dashboard'
+  	get 'dashboard',            to: 'dashboard#show',            as: 'dashboard'
 
-    get 'facilities',      to: 'facilities#index',  as: 'facilities'
-    post 'facilities',     to: 'facilities#import', as: 'import_facilities'
+    get 'facilities',           to: 'facilities#index',          as: 'facilities'
+    post 'facilities',          to: 'facilities#import',         as: 'import_facilities'
   end
 
-  get 'facilities',        to: 'facilities#index',  as: 'facilities'
-  get 'facilities/:id',    to: 'facilities#show',   as: 'facility'
-  get 'facilities/share/:id',  to: 'facilities#share_facility', as: 'share_facility'
-  post 'facilities/share',    to: 'facilities#share', as: 'facilities_share'
-  post 'facility/remove/:id', to: 'users#remove_facility', as: 'remove_facility'
-  get 'contact',           to: 'home#contact',      as: 'contact'
-  post 'contact',          to: 'home#send_contact', as: 'contact_us'
-  get 'user',              to: 'users#show',        as: 'user'
-  get 'articles',          to: 'articles#index',    as: 'articles'
-  get 'articles/share/:title',    to: 'articles#show',     as: 'articles_show'
-  post 'articles/share',   to: 'articles#share',    as: 'articles_share'
-  get 'therapists',        to: 'therapists#index',  as: 'therapists'
+  get  'facilities',            to: 'facilities#index',          as: 'facilities'
+  get  'facilities/:id',        to: 'facilities#show',           as: 'facility'
+  get  'facilities/share/:id',  to: 'facilities#share_facility', as: 'share_facility'
+  post 'facilities/share',      to: 'facilities#share',          as: 'facilities_share'
+  post 'facility/save/:id',     to: 'facilities#save_facility',  as: 'save_facility'
+  post 'facility/remove/:id',   to: 'users#remove_facility',     as: 'remove_facility'
 
-  post 'facility/save/:id',to: 'facilities#save_facility', as: 'save_facility'
+  get  'contact',               to: 'home#contact',              as: 'contact'
+  post 'contact',               to: 'home#send_contact',         as: 'contact_us'
+
+  get  'user',                  to: 'users#show',                as: 'user'
+
+  get  'articles',              to: 'articles#index',            as: 'articles'
+  get  'articles/share/:title', to: 'articles#show',             as: 'articles_show'
+  post 'articles/share',        to: 'articles#share',            as: 'articles_share'
+
+  get  'therapists',            to: 'therapists#index',          as: 'therapists'
 end
