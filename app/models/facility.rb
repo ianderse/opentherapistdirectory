9507518b8bff68bf34699c9588a9a4fac73e5ba8
Ian Andersen <ianderse@mac.com>
2015-03-05 13:59:09 -0700

    style facility show page; pull logic out of view for address

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,6 +1,38 @@
 class Facility < ActiveRecord::Base
   require 'csv'
 
+  def address
+    "#{location_street1} #{location_street2}, #{location_city}, #{location_state} #{location_zip}"
+  end
+
+  def website?
+    if website
+      website
+    else
+      "None Provided"
+    end
+  end
+
+  def intake
+    if intake1
+      intake1
+    elsif intake2
+      intake2
+    else
+      "None Provided"
+    end
+  end
+
+  def hotline
+    if hotline1
+      hotline1
+    elsif hotline2
+      hotline2
+    else
+      "None Provided"
+    end
+  end
+
   def services
     ("<li>" + self.services_text1.to_s + "</li><li>" + self.services_text2.to_s + "</li><li>" + self.services_text3.to_s + "</li><li>" + self.services_text4.to_s + "</li><li>" + self.services_text5.to_s + "</li><li>" + self.services_text6.to_s + "</li><li>" + self.services_text7.to_s + "</li>").gsub(/;/, "").html_safe
   end
